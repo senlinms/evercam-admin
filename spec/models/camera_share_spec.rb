c41b2198734d22914fbee659c318484f5aaa8ac5
Piotr Brudny <pbrudny@gmail.com>
2015-06-22 14:20:08 +0200

    Added camera_share model details with specs

--- spec/models/camera_share_spec.rb
+++ spec/models/camera_share_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe CameraShare, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :camera_id }
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :kind }
+    it { should validate_length_of(:kind).is_at_most(50) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:camera) }
+    it { should belong_to(:user) }
+    it { should belong_to(:sharer).class_name('User') }
+  end
+
+  it 'has a valid factory' do
+    expect(build(:camera_share)).to be_valid
+  end
 end
