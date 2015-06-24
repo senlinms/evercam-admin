acde360a9101f8dc28474554b84b44f0fd6272a2
Piotr Brudny <pbrudny@gmail.com>
2015-06-24 17:24:35 +0200

    Added camera decorator

---
+++ app/decorators/camera_decorator.rb
@@ -0,0 +1,27 @@
+class CameraDecorator < Draper::Decorator
+  delegate :exid, :name
+
+  def vendor_model_name
+    object.vendor_model.try(:name)
+  end
+
+  def vendor_name
+    object.vendor_model.vendor.name if camera.vendor_model.present?
+  end
+
+  def creation_date
+    object.created_at.strftime("%d/%m/%y")
+  end
+
+  def last_poll_date
+    object.last_polled_at.strftime("%d/%m/%y") if object.last_polled_at.present?
+  end
+
+  def is_online
+    h.human_boolean(object.is_online?)
+  end
+
+  def is_public
+    h.human_boolean(camera.is_public?)
+  end
+end
