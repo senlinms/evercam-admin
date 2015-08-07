b49fca8781d5d6b91f14b60e3ac22a9cccc72e72
Azhar Malik <azharmalik02@gmail.com>
2015-08-07 15:50:30 +0500

    Use evercam database instead of separate

--- app/models/camera_share.rb
+++ app/models/camera_share.rb
@@ -1,7 +1,7 @@
 class CameraShare < ActiveRecord::Base
   belongs_to :camera
   belongs_to :user
-  belongs_to :sharer, class: User
+  belongs_to :sharer, :foreign_key => 'sharer_id', :class_name => 'User' #class: User, key: :sharer_id
 
   validates :camera_id, presence: true
   validates :user, presence: true
--- db/migrate/20150623163156_rename_model_id_to_vendor_model_id_in_camera.rb
+++
@@ -1,5 +0,0 @@
-class RenameModelIdToVendorModelIdInCamera < ActiveRecord::Migration
-  def change
-    rename_column :cameras, :model_id, :vendor_model_id
-  end
-end
