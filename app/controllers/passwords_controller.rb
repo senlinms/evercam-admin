e839bb28f058687bf55e66c3b08506f190a54585
Junaid Farooq <junaid@evercam.io>
2015-12-14 12:54:25 +0500

    removed spaces.

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,7 +2,7 @@ class PasswordsController < Devise::PasswordsController
 
 	protected
 
-  def after_sending_reset_password_instructions_path_for(*)
-    new_user_password_path
-  end
+	def after_sending_reset_password_instructions_path_for(*)
+		new_user_password_path
+    end
 end
