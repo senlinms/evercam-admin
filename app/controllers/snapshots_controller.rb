2448d036b318c17b1f0ccbb38b48a4a65820ab66
Junaid Farooq <junaid@evercam.io>
2016-01-06 10:52:18 +0500

    Changed report

--- app/controllers/snapshots_controller.rb
+++ app/controllers/snapshots_controller.rb
@@ -2,6 +2,6 @@ class SnapshotsController < ApplicationController
   before_action :authorize_admin
 
   def index
-    @cameras = Camera.all.includes(:user)
+  	@cameras = Camera.all.includes(:user)
   end
 end
