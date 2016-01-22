a63481675f6e5505e0fe7c6c54d65775c392b9ab
Junaid Farooq <junaid@evercam.io>
2016-01-22 11:30:09 +0500

    Added add

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 # every 1.day, :at => '12:00 am' do
 #   runner "SnapshotReport.add_report", environment: :production
 # end
-every :firday, :at => '12pm' do # Use any day of the week or :weekend, :weekday
+every 6.hours do
   runner "SnapshotReport.add_report", environment: :production
 end
