a326ce8906b015534d53793e203e94a6d6dc038d
Junaid Farooq <junaid@evercam.io>
2016-01-22 11:18:11 +0500

    query changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
-every 1.day, :at => '12:00 am' do
+# every 1.day, :at => '12:00 am' do
+#   runner "SnapshotReport.add_report", environment: :production
+# end
+every :firday, :at => '12pm' do # Use any day of the week or :weekend, :weekday
   runner "SnapshotReport.add_report", environment: :production
 end
