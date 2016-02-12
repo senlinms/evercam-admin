1b3e28ee3d4694d3134ed804d7d21723ec7ab2b1
Junaid Farooq <junaid@evercam.io>
2016-02-12 18:49:01 +0500

    tab identations

--- app/controllers/snapshot_extractors_controller.rb
+++ app/controllers/snapshot_extractors_controller.rb
@@ -14,12 +14,12 @@ class SnapshotExtractorsController < ApplicationController
   def create
   	status = 1
   	SnapshotExtractor.create(
-  		camera_id: params[:camera_id],
-  		from_date: params[:from_date],
-  		to_date: params[:to_date],
-  		interval: params[:interval],
-  		schedule: params[:schedule]
-  		)
+			camera_id: params[:camera_id],
+			from_date: params[:from_date],
+			to_date: params[:to_date],
+			interval: params[:interval],
+			schedule: params[:schedule]
+  	)
   	render json: status
   end
 end
