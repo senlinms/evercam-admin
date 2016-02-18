70a7a0345529598a124f31925a632fc003ad5f3b
Junaid Farooq <junaid@evercam.io>
2016-02-18 18:02:21 +0500

    changed cron job time

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 1.day, :at => '12:00 am' do
   runner "SnapshotReport.add_report", environment: :production
 end
 
-every 30.minutes do
+every 1.day, :at => '04:00 am' do
 	runner "SnapshotExtractor.extract_snapshots", environment: :production
 end
