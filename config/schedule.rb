4d046b1ead62b8f93d7db63b34dd6156277eea88
Junaid Farooq <junaid@evercam.io>
2016-02-25 12:56:38 +0500

    Comentout cronjob

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 1.day, :at => '12:00 am' do
   runner "SnapshotReport.add_report", environment: :production
 end
 
-every 10.minutes do
-	runner "SnapshotExtractor.extract_snapshots", environment: :production
-end
+# every 10.minutes do
+# 	runner "SnapshotExtractor.extract_snapshots", environment: :production
+# end
