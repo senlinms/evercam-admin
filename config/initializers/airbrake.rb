bb612d168f63f1e5545d06248fa72f831b666fca
Junaid Farooq <junaid@evercam.io>
2016-03-02 13:54:59 +0500

    Downgrade Airbrake version for travis

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,3 @@
-require "evercam_misc"
 Airbrake.configure do |config|
-  config.ignore_environments = %w(development test)
-  config.project_id = ENV["AIRBRAKE_ID"].to_i
-  config.project_key = ENV["AIRBRAKE_KEY"]
+  config.api_key = ENV["AIRBRAKE_KEY"]
 end
