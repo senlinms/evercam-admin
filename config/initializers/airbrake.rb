f815f3f0f1ce414d65b1e8f5cca9a5d94467e4e1
Junaid Farooq <junaid@evercam.io>
2016-03-02 12:48:08 +0500

    Added double qouted string

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,5 @@
-require 'evercam_misc'
+require "evercam_misc"
 Airbrake.configure do |config|
-  config.project_id = ENV['AIRBRAKE_ID'].to_i
-  config.project_key = ENV['AIRBRAKE_KEY']
+  config.project_id = ENV["AIRBRAKE_ID"].to_i
+  config.project_key = ENV["AIRBRAKE_KEY"]
 end
