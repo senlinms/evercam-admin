7d2502b7b279f83a5d76b34ce711d060885e6aab
Junaid Farooq <junaid@evercam.io>
2016-06-23 11:30:54 +0500

    Added association to Archive model for users

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -2,4 +2,5 @@ class Archive < ActiveRecord::Base
   establish_connection "evercam_db_#{Rails.env}".to_sym
 
   belongs_to :camera
+  belongs_to :user, :foreign_key => 'requested_by', :class_name => 'EvercamUser'
 end
