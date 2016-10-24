67e4fc5f0bf0da63dc8c013193f38a478ad90673
Junaid Farooq <junaid@evercam.io>
2016-10-24 15:34:12 +0500

    Changed Interval values & enlist all construction cameras (#251)

--- app/controllers/snapshot_extractors_controller.rb
+++ app/controllers/snapshot_extractors_controller.rb
@@ -1,23 +1,23 @@
 class SnapshotExtractorsController < ApplicationController
 
   def index
-  	@cameras = Camera.all.includes(:user, vendor_model: [:vendor]).decorate
-  	@set_cameras = ["Select Camera", ""]
-  	@cameras.each do |camera|
-      @set_cameras[@set_cameras.length] = [camera.exid, camera.id]
-  	end
-  	@set_cameras
+    @cameras = Camera.where(owner_id: 13959).decorate
+    @set_cameras = ["Select Camera", ""]
+    @cameras.each do |camera|
+      @set_cameras[@set_cameras.length] = [camera.name, camera.id]
+    end
+    @set_cameras
   end
 
   def create
-  	status = 1
-  	SnapshotExtractor.create(
-			camera_id: params[:camera_id],
-			from_date: params[:from_date],
-			to_date: params[:to_date],
-			interval: params[:interval],
-			schedule: params[:schedule]
-  	)
-  	render json: status
+    status = 1
+    SnapshotExtractor.create(
+      camera_id: params[:camera_id],
+      from_date: params[:from_date],
+      to_date: params[:to_date],
+      interval: params[:interval],
+      schedule: params[:schedule]
+    )
+    render json: status
   end
 end
