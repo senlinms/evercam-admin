a87139d2403e54a9fd5645757bb96a7190efa121
Junaid Farooq <junaid@evercam.io>
2017-12-28 12:25:51 +0500

    Updated schema.rb file.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,62 +15,61 @@ ActiveRecord::Schema.define(version: 20160428105720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "archives", force: :cascade do |t|
+  create_table "archives", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "countries", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "iso3166_a2", null: false
+  create_table "countries", id: :serial, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "iso3166_a2", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "snapshot_extractors", force: :cascade do |t|
+  create_table "snapshot_extractors", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "snapshot_reports", force: :cascade do |t|
+  create_table "snapshot_reports", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "timelapses", force: :cascade do |t|
+  create_table "timelapses", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "firstname",                              null: false
-    t.string   "lastname",                               null: false
-    t.string   "username",                               null: false
-    t.integer  "country_id"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "firstname", null: false
+    t.string "lastname", null: false
+    t.string "username", null: false
+    t.integer "country_id"
     t.datetime "confirmed_at"
-    t.string   "email",                                  null: false
-    t.string   "reset_token"
+    t.string "email", null: false
+    t.string "reset_token"
     t.datetime "token_expires_at"
-    t.string   "api_id"
-    t.string   "api_key"
-    t.boolean  "is_admin",               default: false, null: false
-    t.string   "stripe_customer_id"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+    t.string "api_id"
+    t.string "api_key"
+    t.boolean "is_admin", default: false, null: false
+    t.string "stripe_customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.index ["country_id"], name: "index_users_on_country_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["country_id"], name: "index_users_on_country_id", using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "users", "countries"
 end
