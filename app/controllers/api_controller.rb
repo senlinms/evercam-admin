2dbdd3f209f42840bda343c1f930b27a209b4263
Azhar Malik <azharmalik02@gmail.com>
2018-07-23 10:59:02 +0500

    Used input param in query

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,7 +6,7 @@ class ApiController < ApplicationController
     cameras = []
 
     if params["shared-with"].present?
-      cameras = Camera.connection.select_all("select distinct c.id,c.exid,c.name,c.timezone,c.is_online,c.is_public,c.created_at,c.last_online_at,
+      cameras = Camera.connection.select_all("select distinct c.id,c.exid,c.name,c.is_online,c.is_public,c.created_at,c.last_online_at,
         cr.frequency,cr.storage_duration,u.api_id,u.api_key,
         (select count(id) as total from camera_shares cs where c.id=cs.camera_id) as total_share,
         (select count(*) from snapmail_cameras sc where sc.camera_id = c.id) snapmail_count
@@ -14,7 +14,7 @@ class ApiController < ApplicationController
         inner join users u on u.id=c.owner_id
         left join cloud_recordings cr on c.id=cr.camera_id
         where cs.user_id in (select id from users where email like '%#{params["shared-with"]}')
-        or c.owner_id in (select id from users where email like '%azhar@evercam.io')")
+        or c.owner_id in (select id from users where email like '%#{params["shared-with"]}')")
     end
     render json: {data: cameras}
   end
